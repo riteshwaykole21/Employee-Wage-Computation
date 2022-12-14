c265707e4b09d43babd7c06ba50e250255c155c6
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-14 21:56:55 +0530

    [Ritesh]add :Solving Using Switch Case

--- EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,34 +9,38 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWageCalculation
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
+
         public static void CheckAttendances()
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HR = 20;
 
             int empHrs = 0;
             int empWages = 0;
 
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                 empHrs = 0;
-            }
+
             empWages = empHrs * EMP_RATE_PER_HR;
             Console.WriteLine("EmpWage:" + empWages);
         }
     }
-}              
+}   
+
+            
             
             
         
