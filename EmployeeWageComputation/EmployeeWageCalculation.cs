c57e64c9fc2c020b7b6eac5169c65685e0f52802
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-14 21:09:42 +0530

    [Ritesh]add :Calculate Part Time Employee Wage

--- EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -10,30 +10,36 @@ namespace EmployeeWageComputation
     {
         public static void CheckAttendances()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HR = 20;
 
             int empHrs = 0;
             int empWages = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
-
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is a Present");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
                 empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                 empHrs = 0;
             }
             empWages = empHrs * EMP_RATE_PER_HR;
             Console.WriteLine("EmpWage:" + empWages);
         }
     }
-}           
+}              
+            
+            
+        
+          
                 
         
     
