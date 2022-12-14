77af035eea6f4a70002a2904dea8d2d5455192ea
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-14 20:23:04 +0530

    [Ritesh]add :Calculate Daily Employee Wage

--- EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -9,19 +9,32 @@ namespace EmployeeWageComputation
     public class EmployeeWageCalculation
     {
         public static void CheckAttendances()
-        { 
+        {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
             Random random = new Random();
             int empCheck = random.Next(2);
 
-            if(empCheck==IS_FULL_TIME)
-            { 
-                  Console.WriteLine("Employee is a Present");
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is a Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWages = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("EmpWage:" + empWages);
         }
     }
-}
+}           
+                
+        
+    
+
