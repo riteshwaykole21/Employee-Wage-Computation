9781b301e472ac63de2816bf8acc33d150cf8d17
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-15 21:17:46 +0530

    [Ritesh]add :calculating Wage For Months

--- EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -12,30 +13,37 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
+        public const int WORKING_DAYS = 20;
 
         public static void CheckAttendances()
         {
 
             int empHrs = 0;
             int empWages = 0;
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
 
-            empWages = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("EmpWage:" + empWages);
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWages = empHrs * EMP_RATE_PER_HR;
+                totalEmpWage = totalEmpWage +empWages;
+                Console.WriteLine("EmpWage:" + empWages);
+             }
+                Console.WriteLine("total emp wage : " +totalEmpWage);
         }
     }
 }   
