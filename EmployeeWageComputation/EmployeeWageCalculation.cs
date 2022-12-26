39bf0e1c23dc9cf92661e6c3e0a07a24d57199db
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-26 20:19:22 +0530

    [Ritesh]add :Mutipal Company Calculation

--- EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -8,53 +8,43 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    public class EmployeeWageCalculation
+    class EmployeeWageCalculation
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTHS = 100;
-
-        public void CheckAttendances()
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2, TOTAL_DAYS = 20, TOTAL_HOUR = 100;
+        int totalSalary, empHr, totalEmpHrs = 0, totalWorkingDays = 0;
+        public int ComputeEmployeeWage(string company, int empRatePrHr, int numOfWorkingDays, int maxHourPrMonth)
         {
-
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTHS && totalWorkingDays < WORKING_DAYS)
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+            while (totalEmpHrs <= maxHourPrMonth && totalWorkingDays <= numOfWorkingDays)
             {
                 totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
-                    case IS_PART_TIME:
-                        empHrs = 4;
+                    case PRESENT:
+                        this.empHr += FULL_DAY_HOUR;
                         break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
+                    case HALF_DAY:
+                        this.empHr += HALF_DAY_HOUR;
                         break;
                     default:
-                        empHrs = 0;
+                        Console.WriteLine("Employee is Absent for the Day");
                         break;
                 }
+                totalEmpHrs += empHr;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHr);
 
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs :" + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("total emp wage : " + totalEmpWage);
+            this.totalSalary = WAGE_PR_HR * totalEmpHrs;
+            Console.WriteLine("Here is the Monthly Wage of Emp." + this.totalSalary);
+            return totalSalary;
         }
     }
-}   
+}
 
-            
-            
-            
-        
-          
-                
-        
-    
+
+
+
+
+
 
